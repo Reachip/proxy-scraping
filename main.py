d802b4afc3176f5881ae61eccf7edf0435cd710f
Rached Mejri <rachedmejri74100@laposte.net>
2018-09-02 17:07:55 +0200

    Adding requirements.txt

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import re
 import asyncio
+import aiosqlite
 import aiohttp
 import aiofiles
 
 
-async def extract_html_page(loop, proxy_collection_from_websites):
-    http_proxy_web_content = []
-    socks_proxy_web_content = []
+async def make_request(websites):
+    web_content = []
 
     async with aiohttp.ClientSession() as session:
-        async with session.get(proxy_collection_from_websites["http"]) as response:
-            http_proxy_web_content.append(await response.text())
+        for website in websites:
+            async with session.get(website) as response:
+                web_content.append(await response.text())
 
-        async with session.get(proxy_collection_from_websites["socks"]) as response:
-            socks_proxy_web_content.append(await response.text())
+    return web_content
 
+
+async def extract_html_page(loop, proxy_collection_from_websites):
+    http_proxy_web_content = await make_request(proxy_collection_from_websites["http"])
+    socks_proxy_web_content = await make_request(
+        proxy_collection_from_websites["socks"]
+    )
 
     return http_proxy_web_content, socks_proxy_web_content
 
@@ -44,7 +50,9 @@ async def extract_ip_adress_from_html(loop, proxy_collection_from_websites):
 
 async def write_ip_in_file(loop, proxy_collection_from_websites):
 
-    http_proxy_ip, socks_proxy_ip = await extract_ip_adress_from_html(loop, proxy_collection_from_websites)
+    http_proxy_ip, socks_proxy_ip = await extract_ip_adress_from_html(
+        loop, proxy_collection_from_websites
+    )
 
     async with aiofiles.open("proxy-list.txt", mode="w+") as txt_file:
         await txt_file.write("========= http proxy ============ \n")
@@ -54,13 +62,18 @@ async def write_ip_in_file(loop, proxy_collection_from_websites):
 
 
 async def main(loop, proxy_collection_from_websites):
-    await asyncio.wait([
-        write_ip_in_file(loop, proxy_collection_from_websites)
-    ])
+    await asyncio.wait([write_ip_in_file(loop, proxy_collection_from_websites)])
+
 
 proxy_collection_from_websites = {
-    "http": "https://free-proxy-list.net/",
-    "socks": "https://www.socks-proxy.net/",
+    "http": [
+        "https://free-proxy-list.net/",
+        "http://www.gatherproxy.com/",
+        "https://proxyhttp.net/",
+        "http://spys.one/en/",
+        "http://proxy-list.org/french/index.php",
+    ],
+    "socks": ["https://www.socks-proxy.net/", "https://sockslist.net/"],
 }
 
 loop = asyncio.get_event_loop()
