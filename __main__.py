c67885479e84d54bce8960587db25532044c90d8
Reachip <r.mejri74100@gmail.com>
2019-03-11 00:19:46 +0100

    Review of the asynchronous logic

--- __main__.py
+++ __main__.py
@@ -1,4 +1,6 @@
 import re
+import json
+
 import asyncio
 import aiohttp
 import aiofiles
@@ -22,33 +24,28 @@ async def extract_ip_adress_from_html(loop, html):
     return http_proxy_ip
 
 
-async def write_ip_in_file(loop, ips):
+async def write_ip_in_file(ips):
     async with aiofiles.open("proxy-list.txt", mode="w+") as txt_file:
         [await txt_file.write("\n".join(ip)) for ip in ips]
 
 
-async def fetch_ips_from_website(loop, website):
+async def fetch_ips_from_website(website):
     html = await make_request(website)
-    ips = await extract_ip_adress_from_html(loop, html)
-    await write_ip_in_file(loop, ips)
+    ips = await extract_ip_adress_from_html(asyncio.get_event_loop(), html)
+
+    await write_ip_in_file(ips)
 
 
-async def main(loop, websites):
+async def main(websites):
     tasks = [
-        loop.create_task(fetch_ips_from_website(loop, website)) for website in websites
+        asyncio.get_event_loop().create_task(fetch_ips_from_website(website)) for website in websites
     ]
 
-    for task in tasks:
-        await task
+    [await task for task in tasks]
 
-
-proxy_collection_websites = (
-    "https://free-proxy-list.net/",
-    "http://www.gatherproxy.com/",
-    "https://proxyhttp.net/",
-    "http://spys.one/en/",
-    "http://proxy-list.org/french/index.php",
-)
-
+with open("datas.json") as f:
+    datas = json.loads(f.read())
+    proxy_collection_websites = datas["websites"]
+    
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main(loop, proxy_collection_websites))
+loop.run_until_complete(main(proxy_collection_websites))
